a42ac9d29120d99d8eb8292f679477cfffba036b
Unknown <k.ballagher@gmail.com>
2018-01-31 15:47:13 +0000

    Random Spawn System
    1st Implementation of the random enemy spawn system.

---
+++ Project Godai/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+    public GameObject[] enemyPrefabArray;
+    public GameObject enemySpawn1;
+    public GameObject enemy1;
+
+    private int randomChoice;
+
+	// Use this for initialization
+	void Start () {
+        SpawnerSetup();
+        RandomiseSpawn();
+        SpawnEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SpawnerSetup()
+    {
+        enemyPrefabArray = Resources.LoadAll<GameObject>("EnemyPrefabs");
+        enemySpawn1 = GameObject.Find("EnemySpawn1");
+    }
+
+    void RandomiseSpawn()
+    {
+        randomChoice = Random.Range(0, enemyPrefabArray.Length);
+        enemy1 = enemyPrefabArray[randomChoice];
+        
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject SpawnedEnemy = Instantiate(enemy1, new Vector3(0, 0, 0), Quaternion.identity, enemySpawn1.transform);
+        SpawnedEnemy.name = enemy1.name;
+    }
+
+}
