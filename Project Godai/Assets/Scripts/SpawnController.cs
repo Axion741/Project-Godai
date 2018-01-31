a9088386b2f75d749678d1afd39382752e6de77c
Unknown <k.ballagher@gmail.com>
2018-01-31 16:27:39 +0000

    Random Spawn - Upto 3 Enemies
    Expanded the random spawn system to accomodate upto 3 spawn points that can hold randomised enemies.

--- Project Godai/Assets/Scripts/SpawnController.cs
+++ Project Godai/Assets/Scripts/SpawnController.cs
@@ -6,15 +6,19 @@ public class SpawnController : MonoBehaviour {
 
     public GameObject[] enemyPrefabArray;
     public GameObject enemySpawn1;
+    public GameObject enemySpawn2;
+    public GameObject enemySpawn3;
     public GameObject enemy1;
-
+    public GameObject enemy2;
+    public GameObject enemy3;
     private int randomChoice;
+    private int enemyCount;
 
 	// Use this for initialization
 	void Start () {
         SpawnerSetup();
         RandomiseSpawn();
-        SpawnEnemy();
+        SpawnEnemy(enemyCount);
 	}
 	
 	// Update is called once per frame
@@ -26,19 +30,44 @@ public class SpawnController : MonoBehaviour {
     {
         enemyPrefabArray = Resources.LoadAll<GameObject>("EnemyPrefabs");
         enemySpawn1 = GameObject.Find("EnemySpawn1");
+        enemySpawn2 = GameObject.Find("EnemySpawn2");
+        enemySpawn3 = GameObject.Find("EnemySpawn3");
+        enemyCount = Random.Range(1, 4);
+        print("enemycount = " + enemyCount);
     }
 
     void RandomiseSpawn()
     {
         randomChoice = Random.Range(0, enemyPrefabArray.Length);
         enemy1 = enemyPrefabArray[randomChoice];
-        
+        randomChoice = Random.Range(0, enemyPrefabArray.Length);
+        enemy2 = enemyPrefabArray[randomChoice];
+        randomChoice = Random.Range(0, enemyPrefabArray.Length);
+        enemy3 = enemyPrefabArray[randomChoice];
     }
 
-    void SpawnEnemy()
+    void SpawnEnemy(int enemyCount)
     {
-        GameObject SpawnedEnemy = Instantiate(enemy1, new Vector3(0, 0, 0), Quaternion.identity, enemySpawn1.transform);
-        SpawnedEnemy.name = enemy1.name;
+        GameObject SpawnedEnemy1 = Instantiate(enemy1, enemySpawn1.transform.position, Quaternion.identity, enemySpawn1.transform);
+        SpawnedEnemy1.name = enemy1.name;
+        if(enemyCount == 1)
+        {
+            return;
+        } else
+            {
+            GameObject SpawnedEnemy2 = Instantiate(enemy2, enemySpawn2.transform.position, Quaternion.identity, enemySpawn2.transform);
+            SpawnedEnemy2.name = enemy2.name;
+            if(enemyCount == 2)
+            {
+                return;
+            }
+            else
+                {
+                GameObject SpawnedEnemy3 = Instantiate(enemy3, enemySpawn3.transform.position, Quaternion.identity, enemySpawn3.transform);
+                SpawnedEnemy3.name = enemy3.name;
+                }
+
+        }
     }
 
 }
