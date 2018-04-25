5973e5507db422d9a0930204feb6ecb5c1867fe4
Unknown <k.ballagher@gmail.com>
2018-04-25 20:47:03 +0100

    Stat Screen Updated
    Added P2/3 to stat screen, mod numbers currently broken.

--- Project Godai/Assets/Scripts/ModNumberController.cs
+++ Project Godai/Assets/Scripts/ModNumberController.cs
@@ -11,16 +11,21 @@ public class ModNumberController : MonoBehaviour {
     private int speedMod;
     private int enduranceMod;
     private int spiritMod;
-    private int playerLevel;
+    private int playerLevel;    
     private int statPoints;
     private float experiencePoints;
+    public int selectedCharacter;
 
     private PlayerStats playerStats;
+    private PlayerStats2 playerStats2;
+    private PlayerStats3 playerStats3;
     private SaveData saveData;
 
 	// Use this for initialization
 	void Start () {
         playerStats = GameObject.FindObjectOfType<PlayerStats>();
+        playerStats2 = GameObject.FindObjectOfType<PlayerStats2>();
+        playerStats3 = GameObject.FindObjectOfType<PlayerStats3>();
         saveData = FindObjectOfType<SaveData>();
 	}
 	
@@ -63,43 +68,155 @@ public class ModNumberController : MonoBehaviour {
 
     public void StrengthText()
     {
-        strengthMod = saveData.modStrength1;
-        text.text = ("<Color=#00ff00ff>+" + strengthMod.ToString() + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                strengthMod = saveData.modStrength1;
+                text.text = ("<Color=#00ff00ff>+" + strengthMod.ToString() + "</Color>");
+                break;
+
+            case 2:
+                strengthMod = saveData.modStrength2;
+                text.text = ("<Color=#00ff00ff>+" + strengthMod.ToString() + "</Color>");
+                break;
+
+            case 3:
+                strengthMod = saveData.modStrength3;
+                text.text = ("<Color=#00ff00ff>+" + strengthMod.ToString() + "</Color>");
+                break;
+        }
     }
 
     public void SpeedText()
     {
-        speedMod = saveData.modSpeed1;
-        text.text = ("<Color=#00ff00ff>+" + speedMod.ToString() + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                speedMod = saveData.modSpeed1;
+                text.text = ("<Color=#00ff00ff>+" + speedMod.ToString() + "</Color>");
+                break;
+
+            case 2:
+                speedMod = saveData.modSpeed2;
+                text.text = ("<Color=#00ff00ff>+" + speedMod.ToString() + "</Color>");
+                break;
+
+            case 3:
+                speedMod = saveData.modSpeed3;
+                text.text = ("<Color=#00ff00ff>+" + speedMod.ToString() + "</Color>");
+                break;
+        }
     }
 
     public void EnduranceText()
     {
-        enduranceMod = saveData.modEndurance1;
-        text.text = ("<Color=#00ff00ff>+" + enduranceMod.ToString() + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                enduranceMod = saveData.modEndurance1;
+                text.text = ("<Color=#00ff00ff>+" + enduranceMod.ToString() + "</Color>");
+                break;
+
+            case 2:
+                enduranceMod = saveData.modEndurance2;
+                text.text = ("<Color=#00ff00ff>+" + enduranceMod.ToString() + "</Color>");
+                break;
+
+            case 3:
+                enduranceMod = saveData.modEndurance3;
+                text.text = ("<Color=#00ff00ff>+" + enduranceMod.ToString() + "</Color>");
+                break;
+        }
+
     }
 
     public void SpiritText()
     {
-        spiritMod = saveData.modSpirit1;
-        text.text = ("<Color=#00ff00ff>+" + spiritMod.ToString() + "</Color>");
+
+        switch (selectedCharacter)
+        {
+            case 1:
+                spiritMod = saveData.modSpirit1;
+                text.text = ("<Color=#00ff00ff>+" + spiritMod.ToString() + "</Color>");
+                break;
+
+            case 2:
+                spiritMod = saveData.modSpirit2;
+                text.text = ("<Color=#00ff00ff>+" + spiritMod.ToString() + "</Color>");
+                break;
+
+            case 3:
+                spiritMod = saveData.modSpirit3;
+                text.text = ("<Color=#00ff00ff>+" + spiritMod.ToString() + "</Color>");
+                break;
+        }
+
+
     }
 
     public void LevelText()
     {
-        playerLevel = saveData.playerLevel1;
-        text.text = ("<Color=white> Lvl: " + playerLevel.ToString() + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                playerLevel = saveData.playerLevel1;
+                text.text = ("<Color=white> Lvl: " + playerLevel.ToString() + "</Color>");
+                break;
+
+            case 2:
+                playerLevel = saveData.playerLevel2;
+                text.text = ("<Color=white> Lvl: " + playerLevel.ToString() + "</Color>");
+                break;
+
+            case 3:
+                playerLevel = saveData.playerLevel3;
+                text.text = ("<Color=white> Lvl: " + playerLevel.ToString() + "</Color>");
+                break;
+        }
+
     }
 
     public void ExpText()
     {
-        experiencePoints = saveData.experiencePoints1;
-        text.text = ("<Color=white> Exp: " + experiencePoints.ToString() + "/" + playerStats.experienceThreshold + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                experiencePoints = saveData.experiencePoints1;
+                text.text = ("<Color=white> Exp: " + experiencePoints.ToString() + "/" + playerStats.experienceThreshold + "</Color>");
+                break;
+
+            case 2:
+                experiencePoints = saveData.experiencePoints2;
+                text.text = ("<Color=white> Exp: " + experiencePoints.ToString() + "/" + playerStats2.experienceThreshold + "</Color>");
+                break;
+
+            case 3:
+                experiencePoints = saveData.experiencePoints3;
+                text.text = ("<Color=white> Exp: " + experiencePoints.ToString() + "/" + playerStats3.experienceThreshold + "</Color>");
+                break;
+        }
+
     }
 
     public void StatText()
     {
-        statPoints = saveData.statPoints1;
-        text.text = ("<Color=white> Stat Points: " + statPoints.ToString() + "</Color>");
+        switch (selectedCharacter)
+        {
+            case 1:
+                statPoints = saveData.statPoints1;
+                text.text = ("<Color=white> Stat Points: " + statPoints.ToString() + "</Color>");
+                break;
+
+            case 2:
+                statPoints = saveData.statPoints2;
+                text.text = ("<Color=white> Stat Points: " + statPoints.ToString() + "</Color>");
+                break;
+
+            case 3:
+                statPoints = saveData.statPoints3;
+                text.text = ("<Color=white> Stat Points: " + statPoints.ToString() + "</Color>");
+                break;
+        }
+
     }
 }
--- Project Godai/Assets/Scripts/StatScreenController.cs
+++ Project Godai/Assets/Scripts/StatScreenController.cs
@@ -5,18 +5,36 @@ using UnityEngine.UI;
 
 public class StatScreenController : MonoBehaviour {
 
+    private ModNumberController modController;
     private PlayerStats playerStats;
     private GameObject p2Toggle;
     private GameObject p3Toggle;
+    private GameObject p1Selector;
+    private GameObject p2Selector;
+    private GameObject p3Selector;
+    public GameObject p1Controls;
+    public GameObject p2Controls;
+    public GameObject p3Controls;
+  
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        modController = FindObjectOfType<ModNumberController>();
         p2Toggle = GameObject.Find("Player2Toggle");
         p3Toggle = GameObject.Find("Player3Toggle");
+        p1Selector = GameObject.Find("Player1ToggleSwitch");
+        p2Selector = GameObject.Find("Player2ToggleSwitch");
+        p3Selector = GameObject.Find("Player3ToggleSwitch");
+        p1Controls = GameObject.Find("Player1StatControls");
+        p2Controls = GameObject.Find("Player2StatControls");
+        p3Controls = GameObject.Find("Player3StatControls");
         playerStats = FindObjectOfType<PlayerStats>();
         playerStats.PlayerStatsSetup();
+        modController.selectedCharacter = 1;
+        p2Controls.SetActive(false);
+        p3Controls.SetActive(false);
 
-	}
+    }
 
     private void Update()
     {
@@ -28,20 +46,72 @@ public class StatScreenController : MonoBehaviour {
         if (playerStats.player2recruited == 1)
         {
             p2Toggle.GetComponent<Toggle>().isOn = true;
+            p2Selector.SetActive(true);
         }else if (playerStats.player2recruited == 0)
         {
             p2Toggle.GetComponent<Toggle>().isOn = false;
+            p2Selector.SetActive(false);
         }
 
         if (playerStats.player3recruited == 1)
         {
             p3Toggle.GetComponent<Toggle>().isOn = true;
+            p3Selector.SetActive(true);
         }
         else if (playerStats.player3recruited == 0)
         {
             p3Toggle.GetComponent<Toggle>().isOn = false;
+            p3Selector.SetActive(false);
         }
 
+    }
+
+    public void ToggleActiveCharacter(int character)
+    {
+        switch (character)
+        {
+            case 1:
+                if (p1Controls.activeSelf == true)
+                {
+                    return; 
+                }
+                else
+                {
+                    p1Controls.SetActive(true);
+                    p2Controls.SetActive(false);
+                    p3Controls.SetActive(false);
+                    modController.selectedCharacter = 1;
+                }
+                break;
+
+            case 2:
+                if (p2Controls.activeSelf == true)
+                {
+                    return;
+                }
+                else
+                {
+                    p1Controls.SetActive(false);
+                    p2Controls.SetActive(true);
+                    p3Controls.SetActive(false);
+                    modController.selectedCharacter = 2;
+                }
+                break;
+
+            case 3:
+                if (p3Controls.activeSelf == true)
+                {
+                    return;
+                }
+                else
+                {
+                    p1Controls.SetActive(false);
+                    p2Controls.SetActive(false);
+                    p3Controls.SetActive(true);
+                    modController.selectedCharacter = 3;
+                }
+                break;
+        }
     }
 
 
