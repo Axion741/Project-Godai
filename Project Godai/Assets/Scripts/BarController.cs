4d3a84e00dee4f28f3f1002c096f7abe5d9e669f
Unknown <k.ballagher@gmail.com>
2018-04-27 12:30:42 +0100

    Exp Bar Animation
    Exp bars now animate, and reset after level up to show progress toward next level.

--- Project Godai/Assets/Scripts/BarController.cs
+++ Project Godai/Assets/Scripts/BarController.cs
@@ -6,11 +6,17 @@ using UnityEngine.UI;
 public class BarController : MonoBehaviour {
     
 
-    public float xpFill;
-    private float barSpeed = 4;
+    private float xpFill;
+    private float xpFill2;
+    private float xpFill3;
+    private float barSpeed = 2;
     private float barStart1;
     private float barStart2;
     private float barStart3;
+    private int player1BarRun;
+    private int player2BarRun;
+    private int player3BarRun;
+
 
     public GameObject barParent1;
     public GameObject barParent2;
@@ -37,6 +43,7 @@ public class BarController : MonoBehaviour {
         //EXPBarWin();
         //Debug.Log("AbExp = " + playerStats.experiencePoints);;
         //Debug.Log("AbThresh = " + playerStats.experienceThreshold);
+        RunExpBars();
     }
 
 
@@ -72,25 +79,86 @@ public class BarController : MonoBehaviour {
                 expBar.fillAmount = barStart1;
                 xpFill = playerStats.experiencePoints / playerStats.experienceThreshold;
                 //Debug.Log("xpfv = " + xpFill);
-                expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, xpFill, Time.deltaTime * barSpeed);
+                //expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, xpFill, Time.deltaTime * barSpeed);
+                player1BarRun = 1;
                 break;
 
             case 2:
                 expBar2.fillAmount = barStart2;
-                xpFill = playerStats2.experiencePoints / playerStats2.experienceThreshold;
+                xpFill2 = playerStats2.experiencePoints / playerStats2.experienceThreshold;
                 //Debug.Log("xpfv = " + xpFill);
-                expBar2.fillAmount = Mathf.Lerp(expBar2.fillAmount, xpFill, Time.deltaTime * barSpeed);
+                //expBar2.fillAmount = Mathf.Lerp(expBar2.fillAmount, xpFill2, Time.deltaTime * barSpeed);
+                player2BarRun = 1;
                 break;
 
             case 3:
                 expBar3.fillAmount = barStart3;
-                xpFill = playerStats3.experiencePoints / playerStats3.experienceThreshold;
+                xpFill3 = playerStats3.experiencePoints / playerStats3.experienceThreshold;
                 //Debug.Log("xpfv = " + xpFill);
-                expBar3.fillAmount = Mathf.Lerp(expBar3.fillAmount, xpFill, Time.deltaTime * barSpeed);
+                //expBar3.fillAmount = Mathf.Lerp(expBar3.fillAmount, xpFill3, Time.deltaTime * barSpeed);
+                player3BarRun = 1;
                 break;
         }
 
 
+    }
+
+    //if playerBarRun == 0, player not present. if == 1, animate exp bar
+    private void RunExpBar1()
+    {
+        if(player1BarRun == 1)
+        {
+            if (expBar.fillAmount != 1)
+            {
+                expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, xpFill, Time.deltaTime * barSpeed);
+            }
+            if (expBar.fillAmount == 1)
+            {
+                expBar.fillAmount = 0;
+                EXPBarWin(1);
+            }
+        }
+    }
+
+    private void RunExpBar2()
+    {
+        if (player2BarRun == 1)
+        {
+            if (expBar2.fillAmount != 1)
+            {
+            expBar2.fillAmount = Mathf.Lerp(expBar2.fillAmount, xpFill2, Time.deltaTime * barSpeed);
+            }
+            
+            if (expBar2.fillAmount == 1)
+            {
+                expBar2.fillAmount = 0;
+                EXPBarWin(2);
+            }
+        }
+    }
+
+    private void RunExpBar3()
+    {
+        if (player3BarRun == 1)
+        {
+            if (expBar3.fillAmount != 1)
+            {
+                expBar3.fillAmount = Mathf.Lerp(expBar3.fillAmount, xpFill3, Time.deltaTime * barSpeed);
+            }
+
+            if (expBar3.fillAmount == 1)
+            {
+                expBar3.fillAmount = 0;
+                EXPBarWin(3);
+            }
+        }
+    }
+
+    private void RunExpBars()
+    {
+        RunExpBar1();
+        RunExpBar2();
+        RunExpBar3();
     }
 
 
